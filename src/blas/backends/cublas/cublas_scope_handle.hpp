3dc8a447142da91e93e473accccfba70ae90ec86
Andrew T. Barker <andrew1.barker@intel.com>
2022-03-21 13:25:22 -0700

    [BLAS] fix functional failures in cublas by forcing host_task to wait on the CUDA stream
    Previously, we used interop_task which had a hidden wait on the CUDA stream. With SYCL 2020
    transition to host_task we add it explicitly.

--- src/blas/backends/cublas/cublas_scope_handle.hpp
+++ src/blas/backends/cublas/cublas_scope_handle.hpp
@@ -66,7 +66,7 @@ class CublasScopedContextHandler {
     bool needToRecover_;
     sycl::interop_handle &ih;
     static thread_local cublas_handle<pi_context> handle_helper;
-    CUstream get_stream(const sycl::queue &queue);
+
     sycl::context get_context(const sycl::queue &queue);
 
 public:
@@ -88,6 +88,8 @@ public:
         CUdeviceptr cudaPtr = ih.get_native_mem<sycl::backend::ext_oneapi_cuda>(acc);
         return reinterpret_cast<T>(cudaPtr);
     }
+
+    CUstream get_stream(const sycl::queue &queue);
 };
 
 } // namespace cublas
